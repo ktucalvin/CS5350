b35b4a58b0da5205e29a4131d88fe6b8b95667f3
ktucalvin <ktucalvin@gmail.com>
2021-10-19 13:27:38 -0600

    Converging BGD/SGD
    Poor performance because learning rate isn't adjustable, also it seems
    even sklearn can't fit the concrete data very well

--- DecisionTree/lms.py
+++ DecisionTree/lms.py
@@ -27,32 +27,63 @@ class BatchGradientDescentClassifier():
     def predict(self, input):
         return self.w @ input
 
-def StochasticGradientDescentClassifier():
+class StochasticGradientDescentClassifier():
+    def __init__(self, learning_rate):
+        self.learning_rate = learning_rate
+
     def train(self, X, Y, threshold=1e-6):
         # Initialize w_0
-        self.w = np.zeros(len(X))
-        err = float("inf")
+        self.w = np.zeros(X.shape[1])
+        norm = float("inf")
+
         # For t = 0, 1, 2, ... (until below threshold)
-        while err > threshold:
+        t = 0
+        while norm > threshold:
+            # shuffle examples
+            shuffled = np.random.permutation(X.shape[1])
+            samplesX = X[shuffled]
+            samplesY = Y[shuffled]
+
             # For each training example (x_i, y_i)
-            for xi, yi in zip(X, Y):
-                pass
-                # w_j^{t+1} = w_j^t + r(y_i - w^Tx_i)x_ij
+            for xi, yi in zip(samplesX, samplesY):
+                t += 1
                 # w^{t+1} = w^t + r(y_i - w^T x_i)x_i
+                wnext = self.w + self.learning_rate * (yi - self.w.dot(xi.T)) * xi
+                norm = np.linalg.norm(wnext - self.w)
+                self.w = np.ravel(wnext)
     
     def predict(self, input):
-        pass
+        return self.w @ input
 
 if __name__ == "__main__":
-    # Xtrain, Ytrain, Xtest, Ytest, attributes = datasets.get_concrete_data()
-    Xtrain, Ytrain, Xtest, Ytest, attributes = datasets.get_hw2_data()
+    # Xtrain, Ytrain, Xtest, Ytest, attributes = datasets.get_hw2_data()
+    Xtrain, Ytrain, Xtest, Ytest, attributes = datasets.get_concrete_data()
 
-    bgd = BatchGradientDescentClassifier(2 ** -4)
+    # this implementation will perform poorly because the learning rate isn't adjustable
+
+    bgd = BatchGradientDescentClassifier(2 ** -8)
     bgd.train(np.matrix(Xtrain), np.array(Ytrain), threshold=1e-6)
 
-    predictions = [bgd.predict(input) for input in Xtest]
-    incorrect = len(Ytest) - np.sum(np.isclose(np.array(predictions), np.array(Ytest), atol=1e-6))
-    print(f"incorrect: {incorrect}")
-    print(predictions)
-    print(Ytest)
+    predictions = [bgd.predict(input) for input in Xtrain]
+    incorrect = len(Ytrain) - np.sum(np.isclose(np.array(predictions), np.array(Ytrain), atol=1e-6))
+    print(f"BGD Training error: {incorrect/len(Ytrain)}")
+
+    # predictions = [bgd.predict(input) for input in Xtest]
+    # incorrect = len(Ytest) - np.sum(np.isclose(np.array(predictions), np.array(Ytest), atol=1e-6))
+    # print(f"incorrect: {incorrect/len(Ytest)}")
+    # print(f"predictions: {predictions}")
+    # print(f"correct labels: {Ytest}")
+    # print(f"bgd w = {bgd.w}")
+
+    print()
+
+    sgd = StochasticGradientDescentClassifier(0.001)
+    sgd.train(np.matrix(Xtrain), np.array(Ytrain), threshold=1e-6)
+
+    predictions = [sgd.predict(input) for input in Xtrain]
+    incorrect = len(Ytrain) - np.sum(np.isclose(np.array(predictions), np.array(Ytrain), atol=1e-6))
+    print(f"incorrect: {incorrect/len(Ytrain)}")
+    print(f"predictions: {predictions}")
+    print(f"correct labels: {Ytest}")
+    print(f"sgd w = {sgd.w}")
 
