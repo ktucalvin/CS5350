7208cead71e3ee4f0592edef85e3dd4939f2baa4
ktucalvin <ktucalvin@gmail.com>
2021-10-26 00:28:12 -0600

    More tuning for kaggle

--- kaggle-sklearn.py
+++ kaggle-sklearn.py
@@ -5,7 +5,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.svm import SVC
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.preprocessing import StandardScaler, OneHotEncoder, LabelEncoder
 from sklearn.compose import ColumnTransformer
 
 S, val, attributes, labels = datasets.get_ilp_data(binarize=True)
@@ -17,6 +17,10 @@ X = X[:, :-1]
 Xtest = np.asarray(val).reshape((23842,15))
 Xtest = Xtest[:, 1:]
 
+# To be able to impute missing values
+X[X == "?"] = np.nan
+Xtest[Xtest == "?"] = np.nan
+
 attr = list(attributes.keys())
 numeric_features = ["age", "fnlwgt", "education-num", "capital-gain", "capital-loss", "hours-per-week"]
 numeric_features = [attr.index(feat) for feat in numeric_features]
@@ -27,7 +31,12 @@ numeric_transformer = Pipeline(
     steps=[("imputer", SimpleImputer(strategy="median")), ("scaler", StandardScaler())]
 )
 
-categorical_transformer = OneHotEncoder(handle_unknown="ignore")
+categorical_transformer = Pipeline(
+    steps=[
+        ("encoder", OneHotEncoder(handle_unknown="ignore")),
+        ("imputer", SimpleImputer(strategy="most_frequent", missing_values=np.nan)),
+    ]
+)
 
 preprocessor = ColumnTransformer(
     transformers=[
@@ -37,7 +46,7 @@ preprocessor = ColumnTransformer(
 )
 
 clf = Pipeline(
-    steps=[("preprocessor", preprocessor), ("classifier", MLPClassifier(max_iter=5000))]
+    steps=[("preprocessor", preprocessor), ("classifier", MLPClassifier(max_iter=5000, early_stopping=True))]
 )
 
 clf.fit(X, Y)
@@ -48,7 +57,7 @@ test_predictions = clf.predict(Xtest)
 print(test_predictions)
 
 test_predictions = ["0" if pred == "-1" else pred for pred in test_predictions]
-with open("sklearn-neural-no-early.csv", "a+", encoding="utf8") as log:
+with open("sklearn-neural-imputed-earlystop.csv", "a+", encoding="utf8") as log:
     log.write("ID,Prediction\n")
     id =1
     for pred in test_predictions:
